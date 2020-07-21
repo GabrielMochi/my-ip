e24fb87075265fb7da96dee81736cd077bfc68dc
Gabriel Mochi <gmochi56@icloud.com>
2020-07-21 20:41:48 -0300

    fix: :bug: handling x-forwarded-for as a string and not as an array

--- middleware/ip.js
+++ middleware/ip.js
@@ -1,11 +1,4 @@
 export default function ({ req, store }) {
-  const xForwardedFor = req.headers['x-forwarded-for']
-
-  console.log(xForwardedFor)
-
-  const ip = Array.isArray(xForwardedFor)
-    ? xForwardedFor.split(',').pop()
-    : req.connection.remoteAddress
-
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   store.commit('setIp', ip)
 }
