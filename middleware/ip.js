58bca8e54ac6b5bd61c7823d9277d7c71f07d665
Gabriel Mochi <gmochi56@icloud.com>
2020-07-21 20:37:21 -0300

    chore: :loud_sound: logging x-forwarded-for header

--- middleware/ip.js
+++ middleware/ip.js
@@ -1,6 +1,8 @@
 export default function ({ req, store }) {
   const xForwardedFor = req.headers['x-forwarded-for']
 
+  console.log(xForwardedFor)
+
   const ip = Array.isArray(xForwardedFor)
     ? xForwardedFor.split(',').pop()
     : req.connection.remoteAddress
